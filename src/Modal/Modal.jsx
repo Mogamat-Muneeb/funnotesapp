56e46238fb799d5ca3d49922811536b6f72716d4
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-01-22 18:48:20 +0200

    wip

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -11,21 +11,21 @@ const Modal = ({ show, onChange,onClose, currentUser }) => {
     }
     // console.log(inputValue, "inputValue");
   return (
-    <div className=" ">
+    <div className="">
     <div
-      className={`flex justify-center items-center h-screen w-screen fixed top-0 left-0 overflow-hidden  bg-black bg-opacity-60 transition-opacity duration-300 z-[100]
+      className={`md:flex hidden justify-center items-center h-screen w-screen fixed top-0 left-0 overflow-hidden  bg-black bg-opacity-60 transition-opacity duration-300 z-[100]
          ${
         show ? "block" : "hidden"
       }`}
     >
       <div className="bg-white  h-[200px] md:max-h-[400px]  w-full md:w-[517px] rounded-sm ">
-        <div className=" flex justify-between items-center px-10 pt-2 ">
-          <h2 className="font-semibold text-[#1D1D1D]">Add a  Notes</h2>
-          <button className="shadow  flex items-center justify-center rounded-full p-2 hover:text-white" onClick={onClose}><VscChromeClose className="text-[#1D1D1D]" /></button>
+        <div className="flex items-center justify-between px-5 pt-2 ">
+          <h2 className="font-bold text-[20px] text-[#1D1D1D]">Add a  Note</h2>
+          <button className="flex items-center justify-center p-2 rounded-full shadow-md text-white bg-[#1D1D1D]" onClick={onClose}><VscChromeClose className="text-white" /></button>
         </div>
         <form onSubmit={handleData}>
-            <div className=" px-10 flex flex-col gap-4 mt-5">
-              <div className=" w-full  m-auto ">
+            <div className="flex flex-col gap-4 px-5 mt-5 ">
+              <div className="w-full m-auto ">
               <input
                 type="text"
                 name=""
@@ -39,7 +39,7 @@ const Modal = ({ show, onChange,onClose, currentUser }) => {
               <div className="">
                 <button
                 type="submit"
-                    className='bg-black text-white rounded h-[45px] w-[160px] mt-4'
+                    className='bg-black text-white rounded h-[45px] w-[160px] mt-4 font-semibold'
                     onClick={() => {
                     if (inputValue.length!==0) {
                       onChange(inputValue);
@@ -47,9 +47,51 @@ const Modal = ({ show, onChange,onClose, currentUser }) => {
                     } 
                   }}
                 >
-                  Add Note
+                  Add
                 </button>
               </div>
+            </div>
+        </form>
+      </div>
+    </div>
+    <div
+      className={`flex md:hidden justify-center items-center h-screen w-screen fixed top-0 left-0 overflow-hidden  bg-black bg-opacity-60 transition-opacity duration-300 z-[100]
+         ${
+        show ? "block" : "hidden"
+      }`}
+    >
+      <div className="bg-white  h-[200px] md:max-h-[400px]  w-full md:w-[517px] rounded-sm mx-4 ">
+        <div className="flex items-center justify-between px-5 pt-4 ">
+          <h2 className="font-bold text-[20px] text-[#1D1D1D]">Add a  Note !!</h2>
+          <button className="flex items-center justify-center p-2 rounded-full shadow-md text-white bg-[#1D1D1D]" onClick={onClose}><VscChromeClose className="text-white" /></button>
+        </div>
+        <form onSubmit={handleData}>
+            <div className="flex items-center justify-center gap-4 px-5 mt-12 ">
+           
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="What's happening?"
+                onChange={event => setInputValue(event.target.value)}
+                value={inputValue}
+                className='bg-gray-200 border rounded-sm w-full pl-2 h-[45px] text-black focus:outline-none'
+              />
+          
+
+                <button
+                type="submit"
+                    className='bg-black text-white rounded h-[45px] w-[100px]  font-semibold'
+                    onClick={() => {
+                    if (inputValue.length!==0) {
+                      onChange(inputValue);
+                      setInputValue("");
+                    } 
+                  }}
+                >
+                  Add
+                </button>
+     
             </div>
         </form>
       </div>
