2d4ff7c71887b5dd3898d81eee877ff826b3f807
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-04-06 15:22:53 +0200

    wip

--- src/elements/Notes.jsx
+++ src/elements/Notes.jsx
@@ -146,22 +146,6 @@ function Notes(props) {
   const handleReadLess = () => {
     setShowFullText(-1);
   };
-  // !details)
-
-  //     <div className="flex items-center justify-center h-screen ">
-  //     <div class="flex space-x-2">
-  //  <div aria-label="Loading..." role="status">
-  //    <svg class="h-7 w-7 animate-spin" viewBox="3 3 18 18">
-  //      <path
-  //        class="fill-[#14c4e1]"
-  //        d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"></path>
-  //      <path
-  //        class="fill-gray-800"
-  //        d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"></path>
-  //    </svg>
-  //  </div>
-  // </div>
-  // </div>
 
   return (
     <div className="w-screen h-screen">
@@ -268,7 +252,6 @@ function Notes(props) {
                     >
                       <div className="h-full ">
                         <p className="text-[14px]"> {val.e.slice(0, 100)}</p>
-                        {/* <Link to={`/notes/${val.id}`} className="text-[12px] leading-3">Read More</Link> */}
                         {val.e.length > 170 && (
                           <Link
                             to={`/notes/${val.id}`}
@@ -330,13 +313,6 @@ function Notes(props) {
             details
               .sort((a, b) => b.createdAt - a.createdAt)
               .map((val, id) => {
-                // const date = new Date(
-                //   val.createdAt.seconds * 1000
-                // ).toLocaleDateString("default", {
-                //   day: "numeric",
-                //   month: "short",
-                //   year: "numeric",
-                // });
                 return (
                   <div
                     key={id}
@@ -345,7 +321,6 @@ function Notes(props) {
                   >
                     <div className="h-full ">
                       <p className="text-[14px]"> {val.e.slice(0, 30)}</p>
-                      {/* <Link to={`/notes/${val.id}`} className="text-[12px]">Read More</Link> */}
                       {val.e.length > 170 && (
                         <Link
                           to={`/notes/${val.id}`}
@@ -356,7 +331,6 @@ function Notes(props) {
                       )}
                     </div>
                     <div className="flex items-end justify-end gap-2 ">
-                      {/* <p className="text-[12px] font-medium"> {date}</p> */}
                       <button className="" onClick={() => handleDelete(val)}>
                         <MdDelete />
                       </button>
